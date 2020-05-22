789637dcbb814e935ceda0b49af32d10b095da7b
Ferenc Kovacs <derefer@gmail.com>
2020-05-22 15:10:27 +0200

    Add project files.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement; 
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] private Sprite bgImage;
+    [SerializeField] GameObject exitPanel;
+    [SerializeField] private Transform puzzleField;
+    [SerializeField] private GameObject btn;
+
+    public Sprite[] puzzles;
+
+    public List<Sprite> gamePuzzles = new List<Sprite>();
+
+    public List<Button> btns = new List<Button>();
+
+    private bool firstGuess, secondGuess;
+
+    private int countGuesses;
+    private int countCorrectGuesses;
+    private int gameGuesses;
+    private int firstGuessIndex, secondGuessIndex;
+
+    private string firstGuessPuzzle, secondGuessPuzzle;
+
+    void Awake()
+    {
+        for (int i = 0; i < 18; i++) {
+            GameObject button = Instantiate(btn);
+            button.name = "" + i;
+            button.transform.SetParent(puzzleField, false);
+        }
+
+        puzzles = Resources.LoadAll<Sprite>("Sprites/MemorySprites");
+    }
+
+    void Start()
+    {
+        GetButtons();
+        AddListeners();
+        AddGamePuzzles();
+        Shuffle(gamePuzzles);
+        gameGuesses = gamePuzzles.Count / 2;
+    }
+
+    void GetButtons()
+    {
+        GameObject[] objects = GameObject.FindGameObjectsWithTag("PuzzleButton");
+        for (int i = 0; i < objects.Length; i++) {
+            btns.Add(objects[i].GetComponent<Button>());
+            btns[i].image.sprite = bgImage;
+        }
+    }
+
+    void AddGamePuzzles()
+    {
+        int looper = btns.Count;
+        int index = 0;
+        for (int i = 0; i < looper; i++) {
+            if (index == looper / 2) {
+                index = 0;
+            }
+            gamePuzzles.Add(puzzles[index + 1]);
+            index++;
+        }
+    }
+
+    void AddListeners()
+    {
+        foreach (Button btn in btns) {
+            btn.onClick.AddListener(() => PickAPuzzle());
+        }
+    }
+
+    public void PickAPuzzle()
+    {
+        string name = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name;
+
+        if (!firstGuess) {
+            firstGuessIndex = int.Parse(name);
+            firstGuess = true;
+            firstGuessPuzzle = gamePuzzles[firstGuessIndex].name;
+            btns[firstGuessIndex].image.sprite = gamePuzzles[firstGuessIndex];
+        } else if (!secondGuess) {
+            secondGuessIndex = int.Parse(name);
+            // Avoid double-clicking the same puzzle
+            if (firstGuessIndex == secondGuessIndex) {
+                return;
+            }
+            secondGuess = true;
+            secondGuessPuzzle = gamePuzzles[secondGuessIndex].name;
+            btns[secondGuessIndex].image.sprite = gamePuzzles[secondGuessIndex];
+            countGuesses++;
+            StartCoroutine(CheckIfThePuzzlesMatch());
+        }
+    }
+
+    IEnumerator CheckIfThePuzzlesMatch()
+    {
+        yield return new WaitForSeconds (.5f);
+        if (firstGuessPuzzle == secondGuessPuzzle) {
+            yield return new WaitForSeconds (.1f);
+            btns[firstGuessIndex].interactable = false;
+            btns[secondGuessIndex].interactable = false;
+            btns[firstGuessIndex].image.color = new Color(0, 0, 0, 0);
+            btns[secondGuessIndex].image.color = new Color(0, 0, 0, 0);
+            CheckIfTheGameIsFinished();
+        } else {
+            yield return new WaitForSeconds (.5f);
+            btns[firstGuessIndex].image.sprite = bgImage;
+            btns[secondGuessIndex].image.sprite = bgImage;
+        }
+        firstGuess = secondGuess = false;
+    }
+
+    void CheckIfTheGameIsFinished()
+    {
+        countCorrectGuesses++;
+        if (countCorrectGuesses == gameGuesses) {
+            exitPanel.SetActive(true);
+            string message = "Game finished!\nIt took you <b>" + countGuesses + "</b> guesses to finish the game."; 
+            Debug.Log(message);
+            GameObject.Find("Canvas/Panel Exit/Text").GetComponent<Text>().text = message;
+        }
+    }
+
+    void Shuffle(List<Sprite> list)
+    {
+        for (int i = 0; i < list.Count; i++) {
+            Sprite temp = list[i];
+            int randomIndex = Random.Range(i, list.Count);
+            list[i] = list[randomIndex];
+            list[randomIndex] = temp;
+        }
+    }
+
+    public void Replay()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # MemoryGame
 Basic memory game in Unity
+
+The project was based on this tutorial from "Awesome Tuts": https://www.youtube.com/watch?v=qaCjBh7bWz0&list=PLZhNP5qJ2IA2DA4bzDyxFMs8yogVQSrjW
+
+Additional changes compared to the tutorial:
+..* Fix bug when clicking the same puzzle twice
+..* Make it possible to replay or exit the game in the end
+..* Extend the board from 8 to 18 puzzles using my own photos
+
+This is the first piece of code I wrote (adapted) in C#, so it's probably not following the best practices.
