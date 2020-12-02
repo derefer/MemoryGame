e799bc4da6ab7180388864a91918b9477588fe85
Ferenc Kovacs <derefer@gmail.com>
2020-12-02 21:55:19 +0100

    Improve readability of the code.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
+using static System.Math;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
@@ -9,10 +11,10 @@ using TMPro;
 public class GameController : MonoBehaviour
 {
     // It looks better if it's a square like 4x4...
-    private const int NUM_OF_BUTTONS = 16;
-    private const int NUM_OF_GAME_GUESSES = NUM_OF_BUTTONS / 2;
+    private const int NUM_OF_TILES = 16;
+    private const int NUM_OF_CORRECT_GUESSES = NUM_OF_TILES / 2;
 
-    [SerializeField] private Sprite backImage;
+    [SerializeField] private Sprite backsideSprite;
     [SerializeField] private GameObject panelFinished; // TODO: Better naming
     [SerializeField] private GameObject panelPaused; // TODO: Better naming
     [SerializeField] private GameObject panelImage; // TODO: Better naming
@@ -21,9 +23,8 @@ public class GameController : MonoBehaviour
     [SerializeField] private GameObject buttonPrefab;
     [SerializeField] TextMeshProUGUI timerText, movesText;
 
-    private Sprite[] puzzles;
-    private List<Sprite> gamePuzzles = new List<Sprite>();
-    private List<Button> buttons = new List<Button>();
+    private List<Sprite> tileSprites = new List<Sprite>();
+    private List<Button> tileButtons = new List<Button>();
 
     private bool firstGuess, secondGuess;
 
@@ -39,10 +40,9 @@ public class GameController : MonoBehaviour
 
     private void Start()
     {
-        LoadSprites();
+        InitSprites();
+        ShuffleSprites();
         InitButtons();
-        InitPuzzles();
-        ShufflePuzzles();
 
         Time.timeScale = 1f;
         startTime = Time.time;
@@ -71,17 +71,11 @@ public class GameController : MonoBehaviour
         return minutesString + ":" + secondsString;
     }
 
-    private void LoadSprites()
-    {
-        // Strange, but the "Resources" folder seems to be some kind of Unity convention...
-        puzzles = Resources.LoadAll<Sprite>("Sprites/MemorySprites");
-    }
-
     private void InitButtons()
     {
-        for (int buttonId = 0; buttonId < NUM_OF_BUTTONS; ++buttonId) {
+        for (int buttonId = 0; buttonId < NUM_OF_TILES; ++buttonId) {
             Button button = CreateButton(buttonId);
-            buttons.Add(button);
+            tileButtons.Add(button);
         }
     }
 
@@ -91,30 +85,30 @@ public class GameController : MonoBehaviour
         buttonGameObject.name = buttonId.ToString();
         buttonGameObject.transform.SetParent(puzzleField, false);
         Button button = buttonGameObject.GetComponent<Button>();
-        button.image.sprite = backImage;
+        button.image.sprite = backsideSprite;
         button.onClick.AddListener(() => PickAPuzzle());
         return button;
     }
 
-    private void InitPuzzles()
+    private void InitSprites()
     {
-        int j = 0;
-        for (int i = 0; i < NUM_OF_BUTTONS; ++i) {
-            if (j == NUM_OF_BUTTONS / 2) {
-                j = 0;
-            }
-            gamePuzzles.Add(puzzles[j + 1]);
-            ++j;
+        // The "Resources" folder is a Unity convention
+        Sprite[] tileSprites = Resources.LoadAll<Sprite>("Sprites/MemorySprites");
+        // The backsideSprite needs to be skipped
+        foreach (Sprite tileSprite in tileSprites.Skip(1)) {
+            this.tileSprites.Add(tileSprite);
+            this.tileSprites.Add(tileSprite);
         }
     }
 
-    private void ShufflePuzzles()
+    private void ShuffleSprites()
     {
-        for (int i = 0; i < NUM_OF_BUTTONS; ++i) {
-            Sprite temp = gamePuzzles[i];
-            int randomIndex = Random.Range(i, NUM_OF_BUTTONS);
-            gamePuzzles[i] = gamePuzzles[randomIndex];
-            gamePuzzles[randomIndex] = temp;
+        int numOfTilesToShuffle = Min(tileSprites.Count, NUM_OF_TILES);
+        for (int tileSpriteIndex = 0; tileSpriteIndex < numOfTilesToShuffle; ++tileSpriteIndex) {
+            Sprite temp = tileSprites[tileSpriteIndex];
+            int randomIndex = Random.Range(tileSpriteIndex, numOfTilesToShuffle);
+            tileSprites[tileSpriteIndex] = tileSprites[randomIndex];
+            tileSprites[randomIndex] = temp;
         }
     }
 
@@ -125,8 +119,8 @@ public class GameController : MonoBehaviour
         if (!firstGuess) {
             firstGuessIndex = int.Parse(name);
             firstGuess = true;
-            firstGuessPuzzle = gamePuzzles[firstGuessIndex].name;
-            buttons[firstGuessIndex].image.sprite = gamePuzzles[firstGuessIndex];
+            firstGuessPuzzle = tileSprites[firstGuessIndex].name;
+            tileButtons[firstGuessIndex].image.sprite = tileSprites[firstGuessIndex];
         } else if (!secondGuess) {
             secondGuessIndex = int.Parse(name);
             // Avoid double-clicking the same puzzle
@@ -134,8 +128,8 @@ public class GameController : MonoBehaviour
                 return;
             }
             secondGuess = true;
-            secondGuessPuzzle = gamePuzzles[secondGuessIndex].name;
-            buttons[secondGuessIndex].image.sprite = gamePuzzles[secondGuessIndex];
+            secondGuessPuzzle = tileSprites[secondGuessIndex].name;
+            tileButtons[secondGuessIndex].image.sprite = tileSprites[secondGuessIndex];
             countGuesses++;
             movesText.text = countGuesses.ToString();
             StartCoroutine(CheckIfThePuzzlesMatch());
@@ -147,15 +141,15 @@ public class GameController : MonoBehaviour
         yield return new WaitForSeconds(.5f);
         if (firstGuessPuzzle == secondGuessPuzzle) {
             yield return new WaitForSeconds(.1f);
-            buttons[firstGuessIndex].interactable = false;
-            buttons[secondGuessIndex].interactable = false;
-            buttons[firstGuessIndex].image.color = new Color(0, 0, 0, 0);
-            buttons[secondGuessIndex].image.color = new Color(0, 0, 0, 0);
+            tileButtons[firstGuessIndex].interactable = false;
+            tileButtons[secondGuessIndex].interactable = false;
+            tileButtons[firstGuessIndex].image.color = new Color(0, 0, 0, 0);
+            tileButtons[secondGuessIndex].image.color = new Color(0, 0, 0, 0);
             CheckIfTheGameIsFinished();
         } else {
             yield return new WaitForSeconds(.5f);
-            buttons[firstGuessIndex].image.sprite = backImage;
-            buttons[secondGuessIndex].image.sprite = backImage;
+            tileButtons[firstGuessIndex].image.sprite = backsideSprite;
+            tileButtons[secondGuessIndex].image.sprite = backsideSprite;
         }
         firstGuess = secondGuess = false;
     }
@@ -163,7 +157,7 @@ public class GameController : MonoBehaviour
     private void CheckIfTheGameIsFinished()
     {
         countCorrectGuesses++;
-        if (countCorrectGuesses == NUM_OF_GAME_GUESSES) {
+        if (countCorrectGuesses == NUM_OF_CORRECT_GUESSES) {
             isGameFinished = true;
             // The order matters! TimerText must be active at this point.
             string finishedText = CreateFinishedText();
